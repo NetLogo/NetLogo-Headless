59d5c96460eccdd47c62ad6994a6ff00782d4dd7
Seth Tisue <seth@tisue.net>
2014-10-23 20:33:23 -0400

    wip temporarily disable scalastyle

--- nightly.sh
+++ nightly.sh
@@ -51,8 +51,8 @@ echo "*** done: nogen slow:test"
 if [ ${PIPESTATUS[0]} -ne 0 ] ; then echo "*** FAILED: depend"; exit 1; fi
 echo "*** done: depend"
 
-./sbt scalastyle 2>&1 | tee tmp/nightly/6-scalastyle.txt
-if [ `wc -l < target/scalastyle-result.xml` -ne 2 ] ; then echo "*** FAILED: scalastyle"; exit 1; fi
-echo "*** done: scalastyle"
+#./sbt scalastyle 2>&1 | tee tmp/nightly/6-scalastyle.txt
+#if [ `wc -l < target/scalastyle-result.xml` -ne 2 ] ; then echo "*** FAILED: scalastyle"; exit 1; fi
+#echo "*** done: scalastyle"
 
 echo "****** all done!"
