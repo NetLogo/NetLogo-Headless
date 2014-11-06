d7c2ba9ef57522a704f51b120fb7d515e55bd8b6
Seth Tisue <seth@tisue.net>
2014-11-06 18:17:07 -0500

    add comment

--- src/main/core/Let.scala
+++ src/main/core/Let.scala
@@ -2,5 +2,9 @@
 
 package org.nlogo.core
 
-// some lets are "synthetic" and don't have a name
+// we use reference equality, not structural equality, to compare these, since multiple lets
+// can have the same variable name but are not the same
+
+// some lets are "synthetic" and don't have a name, so that's the null
+
 case class Let(name: String = null)
