f4bff5991ce085c492b551118cc4e7606042d3db
Seth Tisue <seth@tisue.net>
2014-11-06 18:21:50 -0500

    clean up: merge ParserTests into FrontEndTests

--- src/test/compile/front/FrontEndTests.scala
+++ src/test/compile/front/FrontEndTests.scala
@@ -278,4 +278,54 @@ class FrontEndTests extends FunSuite {
       "ReporterApp '[1 2 3]' ")
   }
 
+  /// duplicate name tests
+
+  def duplicateName(s: String, err: String) = {
+    val e = intercept[api.CompilerException] {
+      FrontEnd.frontEnd(s)
+    }
+    assertResult(err)(e.getMessage)
+  }
+
+  test("LetSameNameAsCommandProcedure2") {
+    duplicateName("to b let a 5 end  to a end",
+      "There is already a procedure called A")
+  }
+  test("LetSameNameAsReporterProcedure2") {
+    duplicateName("to b let a 5 end  to-report a end",
+      "There is already a procedure called A")
+  }
+  test("LetNameSameAsEnclosingCommandProcedureName") {
+    duplicateName("to bazort let bazort 5 end",
+      "There is already a procedure called BAZORT")
+  }
+  test("LetNameSameAsEnclosingReporterProcedureName") {
+    duplicateName("to-report bazort let bazort 5 report bazort end",
+      "There is already a procedure called BAZORT")
+  }
+  test("SameLocalVariableTwice1") {
+    duplicateName("to a1 locals [b b] end",
+      "Nothing named LOCALS has been defined.")
+  }
+  test("SameLocalVariableTwice2") {
+    duplicateName("to a2 [b b] end",
+      "There is already a local variable called B here")
+  }
+  test("SameLocalVariableTwice3") {
+    duplicateName("to a3 let b 5 let b 6 end",
+      "There is already a local variable here called B")
+  }
+  test("SameLocalVariableTwice4") {
+    duplicateName("to a4 locals [b] let b 5 end",
+      "Nothing named LOCALS has been defined.")
+  }
+  test("SameLocalVariableTwice5") {
+    duplicateName("to a5 [b] locals [b] end",
+      "Nothing named LOCALS has been defined.")
+  }
+  test("SameLocalVariableTwice6") {
+    duplicateName("to a6 [b] let b 5 end",
+      "There is already a local variable here called B")
+  }
+
 }
