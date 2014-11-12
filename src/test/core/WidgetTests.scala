97bf077fc1a738997237e5d0210d80da91f7ad96
Robert Grider <robert.grider@northwestern.edu>
2014-11-12 17:31:50 -0600

    Enable parsing a widget that has a LogoList as a choice.

---
+++ src/test/core/WidgetTests.scala
@@ -0,0 +1,20 @@
+// (C) Uri Wilensky. https://github.com/NetLogo/NetLogo
+
+package org.nlogo.core
+
+import org.scalatest.FunSuite
+
+class WidgetTests extends FunSuite {
+  test("Chooser handles choices with lists") {
+    val l = new LogoList {
+      val toList = List(1, 2, 3).map(_.toDouble).asInstanceOf[List[AnyRef]]
+      override def toString: String = toList.toString()
+    }
+
+    val chooser = Chooser(
+      display = "FOOBAR",
+      varName = "FOOBAR",
+      choices = List(l, 4.toDouble).asInstanceOf[List[AnyRef]])
+    assertResult("[[1.0 2.0 3.0] 4.0]")(chooser.constraint(1))
+  }
+}
