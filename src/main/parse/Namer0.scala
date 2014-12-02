fbf3d54638e8cb998e2f4d74fa3dc20c595d42ca
Robert Grider <robert.grider@northwestern.edu>
2014-12-02 13:54:00 -0600

    Move keywords from api to core

--- src/main/parse/Namer0.scala
+++ src/main/parse/Namer0.scala
@@ -3,13 +3,13 @@
 package org.nlogo.parse
 
 import org.nlogo.{ core, api },
-  core.{ Token, TokenType }
+  core.{Keywords, Token, TokenType}
 
 object Namer0 extends (Token => Token) {
   override def apply(token: Token) =
     if (token.tpe == TokenType.Bad)
       throw new api.CompilerException(token)
-    else if (api.Keywords.isKeyword(token.text))
+    else if (Keywords.isKeyword(token.text))
       token.copy(tpe = TokenType.Keyword)
     else Constants.get(token.text) match {
       case Some(value) =>
