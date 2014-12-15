1c98b0fa1afb59846c0ee5c7cad5b45097cf442d
Robert Grider <robert.grider@northwestern.edu>
2014-12-15 12:52:51 -0600

    Add agentClassString to Instruction

--- src/main/core/Instruction.scala
+++ src/main/core/Instruction.scala
@@ -5,6 +5,7 @@ package org.nlogo.core
 trait Instruction extends TokenHolder {
   def syntax: Syntax
   var token: Token = null
+  var agentClassString: String = "OTPL"
   def displayName = token.text.toUpperCase
 }
 
