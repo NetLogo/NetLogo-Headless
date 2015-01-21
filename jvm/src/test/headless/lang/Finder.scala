7db60aef3fd3da4d546a8d8abbc07ae8a813c1f3
Robert Grider <robert.grider@northwestern.edu>
2015-01-21 14:41:12 -0600

    build.sbt builds jvmBuild, shared, parserJvm, parserJs
    - Introduce jvmBuild, for building the NetLogoHeadless artifact.
    - Move test/import to jvm/test/import
    - Move test/import-pcolors into jvm/test
    - Move extensions to jvm/extensions
    - Modify tests utilizing extensions to locate extensions correctly in
      new directory structure
    - Modify ExtensionManager to support alternate path for looking up
      extensions
    - Fix degenerate import case when saving with a modelPath of extensions
    - Adds parser-core directory
    - Keep slow parser test in jvm
    - Add jvm-dependent parser files into parserJvm project
    - Update scaladoc to compile across multiple projects
    - jvmBuild depends on parserJvm
    - Split resources into a separate project and move into jvm/resources

---
+++ jvm/src/test/headless/lang/Finder.scala
@@ -0,0 +1,28 @@
+// (C) Uri Wilensky. https://github.com/NetLogo/NetLogo
+
+package org.nlogo.headless
+package lang
+
+import java.io.File
+import org.nlogo.headless.test.{Finder => TestFinder, ExtensionTests, ModelTests, ReporterTests, CommandTests,
+                                AbstractFixture, TestMode, Reporter, Command, Declaration, Open, LanguageTest}
+import org.scalatest.{ FunSuite, Tag }
+
+import
+  org.nlogo.{ api, core, util },
+    api.FileIO.file2String,
+    core.{ Model, Resource },
+    util.SlowTest
+
+trait Finder extends TestFinder {
+  override def withFixture[T](name: String)(body: AbstractFixture => T): T =
+    Fixture.withFixture(name) { fixture =>
+      fixture.workspace.setModelPath("jvm/extensions")
+      body(fixture)
+    }
+}
+
+class TestCommands extends CommandTests with Finder
+class TestReporters extends ReporterTests with Finder
+class TestModels extends ModelTests with Finder
+class TestExtensions extends ExtensionTests with Finder
