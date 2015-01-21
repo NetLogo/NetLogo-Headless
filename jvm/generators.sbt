a5fc373fb892d73d141d147f328c967a8b7daf7c
Robert Grider <robert.grider@northwestern.edu>
2015-01-21 14:41:12 -0600

    build.sbt builds jvmBuild, shared, macros, parserJvm, parserJs
    - Introduce jvmBuild, for building the NetLogoHeadless artifact.
    - Move test/import to jvm/test/import
    - Move test/import-pcolors into jvm/test
    - Move extensions to jvm/extensions
    - Modify tests utilizing extensions to locate extensions correctly in
      new directory structure
    - Modify ExtensionManager to support alternate path for looking up
      extensions
    - Fix degenerate import case when saving with a modelPath of extensions
    - Adds parser-core directory
    - Keep slow parser test in jvm
    - Add jvm-dependent parser files into parserJvm project
    - Update scaladoc to compile across multiple projects
    - jvmBuild depends on parserJvm
    - Split resources into a separate project and move into jvm/resources

---
+++ jvm/generators.sbt
@@ -0,0 +1,28 @@
+///
+/// JFlex
+///
+
+sourceGenerators in Compile += Def.task[Seq[File]] {
+  val src = (sourceManaged in Compile).value
+  val base = baseDirectory.value
+  val s = streams.value
+  val cache =
+    FileFunction.cached(s.cacheDirectory / "lexers", inStyle = FilesInfo.hash, outStyle = FilesInfo.hash) {
+      in: Set[File] =>
+        Set(flex(s.log.info(_), base, src, "ImportLexer"))
+    }
+  cache(Set(base / "project" / "flex" / "warning.txt",
+            base / "project" / "flex" / "ImportLexer.flex")).toSeq
+}.taskValue
+
+def flex(log: String => Unit, base: File, dir: File, kind: String): File = {
+  val project = base / "project" / "flex"
+  val result = dir / (kind + ".java")
+  log("generating " + result)
+  JFlex.Main.main(Array("--quiet", (project / (kind + ".flex")).asFile.toString))
+  IO.write(result,
+    IO.read(project / "warning.txt") +
+    IO.read(project / (kind + ".java")))
+  (project / (kind + ".java")).asFile.delete()
+  result
+}
--- nightly.sh
+++ nightly.sh
@@ -11,16 +11,17 @@ fi
 
 rm -rf tmp/nightly
 mkdir -p tmp/nightly
+mkdir -p jvm/tmp
 
 # here we're using pipes so "-e" isn't enough to stop when something fails.
 # maybe there's an easier way, than I've done it below, I don't know.
 # I suck at shell scripting - ST 2/15/11
 
-./sbt test:compile "ensime generate" 2>&1 | tee tmp/nightly/compile.txt
+./sbt jvmBuild/test:compile "ensime generate" 2>&1 | tee tmp/nightly/compile.txt
 if [ ${PIPESTATUS[0]} -ne 0 ] ; then echo "*** FAILED: test:compile"; exit 1; fi
 echo "*** done: test:compile"
 
-./sbt fast:test 2>&1 | tee tmp/nightly/0-fast-test.txt
+./sbt jvmBuild/fast:test 2>&1 | tee tmp/nightly/0-fast-test.txt
 if [ ${PIPESTATUS[0]} -ne 0 ] ; then echo "*** FAILED: fast:test"; exit 1; fi
 echo "*** done: fast:test"
 
@@ -28,9 +29,9 @@ echo "*** done: fast:test"
 if [ ${PIPESTATUS[0]} -ne 0 ] ; then echo "*** FAILED: nogen fast:test"; exit 1; fi
 echo "*** done: nogen fast:test"
 
-./sbt all 2>&1 | tee tmp/nightly/2-sbt-all.txt
-if [ ${PIPESTATUS[0]} -ne 0 ] ; then echo "*** FAILED: sbt all"; exit 1; fi
-echo "*** done: sbt all"
+./sbt jvmBuild/all 2>&1 | tee tmp/nightly/2-sbt-all.txt
+if [ ${PIPESTATUS[0]} -ne 0 ] ; then echo "*** FAILED: sbt jvmBuild/all"; exit 1; fi
+echo "*** done: sbt jvmBuild/all"
 
 # turning off logBuffered for the slow tests keeps us from going 10
 # minutes without producing any output, which would make Travis time
@@ -39,7 +40,7 @@ echo "*** done: sbt all"
 # will take more than 50 minutes to run (the Travis limit)
 # - ST 8/29/13, 4/1/14
 
-./sbt 'set logBuffered in test := false' slow:test 2>&1 | tee tmp/nightly/3-slow-test.txt
+./sbt 'set logBuffered in test := false' jvmBuild/slow:test 2>&1 | tee tmp/nightly/3-slow-test.txt
 if [ ${PIPESTATUS[0]} -ne 0 ] ; then echo "*** FAILED: slow:test"; exit 1; fi
 echo "*** done: slow:test"
 
@@ -47,11 +48,11 @@ echo "*** done: slow:test"
 if [ ${PIPESTATUS[0]} -ne 0 ] ; then echo "*** FAILED: nogen slow:test"; exit 1; fi
 echo "*** done: nogen slow:test"
 
-./sbt depend 2>&1 | tee tmp/nightly/5-depend.txt
+./sbt jvmBuild/depend 2>&1 | tee tmp/nightly/5-depend.txt
 if [ ${PIPESTATUS[0]} -ne 0 ] ; then echo "*** FAILED: depend"; exit 1; fi
 echo "*** done: depend"
 
-./sbt scalastyle 2>&1 | tee tmp/nightly/6-scalastyle.txt
+./sbt jvmBuild/scalastyle parserCore/scalastyle parserJvm/scalastyle macros/scalastyle parserJs/scalastyle 2>&1 | tee tmp/nightly/6-scalastyle.txt
 if [ `grep error target/scalastyle-result.xml | wc -l` -ne 0 ] ; then echo "*** FAILED: scalastyle"; exit 1; fi
 echo "*** done: scalastyle"
 
