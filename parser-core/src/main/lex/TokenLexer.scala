c6913613915057356da50d5f179787cb50223202
Robert Grider <robert.grider@northwestern.edu>
2015-01-23 09:42:03 -0600

    ParserJs building and passing all tests, parserJvm not compiling

--- parser-core/src/main/lex/TokenLexer.scala
+++ parser-core/src/main/lex/TokenLexer.scala
@@ -2,25 +2,27 @@
 
 package org.nlogo.lex
 
-import java.io.{ Reader => JReader }
+import java.io.{Reader => JReader, BufferedReader}
 
 import org.nlogo.core.{ NumberParser, StringEscaper, Token, TokenType }
+import org.nlogo.lex.TokenLexer.WrappedInput
+import TokenLexer.WrappedInput
+import LexOperations._
 
-import scala.util.{ matching, parsing },
-  matching.Regex,
-  parsing.{ combinator, input },
-    combinator.{ PackratParsers, RegexParsers },
-    input.{ Reader, StreamReader }
+import scala.annotation.tailrec
+import scala.collection.immutable.HashSet
 
-abstract class TokenLexer extends RegexParsers with PackratParsers {
-  import TokenLexer.{ TokenParseResult, WrappedInput }
+class TokenLexer {
+  import LexOperations.PrefixConversions._
 
-  private val validIdentifierChars = """\p{L}\p{Digit}_.?=*!<>:#\+/%$^'&\\-"""
-  private val identifierChar = s"[$validIdentifierChars]"
-  private val badChar = s"[^$validIdentifierChars]"
-  private val stringPattern = """(?:\\\"|\\\\|\\[^"]|[^\r\n\\"])*""".r
-  private val commentPattern = ";[^\\r\\n]*".r
-  private val numberPattern = s"""-?\\.?[0-9]$identifierChar*""".r
+  private val identifierPunctuation = """_.?=*!<>:#+/%$^'&\\-"""
+  private val digits = "0123456789"
+  private val identifierChars = {
+    UnicodeInformation.letterCharacters.foldLeft(HashSet[Int]())(_ ++ _.toSet) ++
+      (digits.map(_.toInt).toSet ++ identifierPunctuation.map(_.toInt).toSet)
+  }
+
+  private def validIdentifierChar(c: Char): Boolean = identifierChars.contains(c)
 
   private val punctuation = Map(
     "," -> TokenType.Comma,
@@ -32,156 +34,198 @@ abstract class TokenLexer extends RegexParsers with PackratParsers {
     "]" -> TokenType.CloseBracket
   )
 
-  private[lex] def tokenAndSkippedSpaceParser: PackratParser[TokenParseResult]
+  def apply(input: WrappedInput): (Token, WrappedInput) = {
+    val (wsCount, remainder) = fastForwardWhitespace(input)
+    if (input.hasNext) {
+      val r = Seq(extensionLiteral, punct, comment, numericLiteral, string, ident, illegalCharacter)
+        .foldLeft((Option.empty[Token], input)) {
+        case ((Some(token), remaining), (prefixDetector, tokenizer)) => (Some(token), remaining)
+        case ((None, remaining), (prefixDetector, tokenizer)) =>
+          remaining.assembleToken(prefixDetector, tokenizer)
+            .map(o => (Some(o._1), o._2))
+            .getOrElse((None, remaining))
+      }
+      (r._1.get, r._2)
+    } else
+      (Token.Eof, input)
+  }
 
-  private[lex] def token: PackratParser[TokenParseResult] =
-    extensionLiteral | punct | comment| number | ident | string | illegalCharacter
+  def fastForwardWhitespace(input: WrappedInput): (Int, WrappedInput) = {
+    val (spaces, remainder) = input.longestPrefix({
+      c => if (Character.isWhitespace(c)) Accept else Finished
+    })
+    (spaces.length, remainder)
+  }
 
-  private def extensionLiteral: PackratParser[TokenParseResult] = completeLiteral | unclosedLiteral
+  class DoubleBracePairMatcher extends LexPredicate {
+    var lastChar = Option.empty[Char]
+    var nesting = 0
+    val detectEnd = withFeedback[(Option[Char], Int)]((lastChar, nesting)) {
+        case ((_, n), _) if n < 0 => ((None, n), Error)
+        case ((_, n), c) if c == '\r' || c == '\n' => ((Some(c), n), Finished)
+        case ((Some('}'), 1), '}') => ((None, 0), Finished)
+        case ((Some('{'), n), '{') => ((None, n + 1), Accept)
+        case ((Some('}'), n), '}') => ((None, n - 1), Accept)
+        case ((_, n), c)           => ((Some(c), n), Accept)
+        case _ => ((None, 0), Error)
+      }
 
-  private def unclosedLiteral: PackratParser[TokenParseResult] =
-    "{{" ~! ".*".r ~ "\\r|\\n".r.? ^^ {
-      case _ ~ s ~ Some(_) =>
-        TokenParseResult("", TokenType.Bad, "End of line reached unexpectedly")
-      case _ =>
-        TokenParseResult("", TokenType.Bad, "End of file reached unexpectedly")
+    def apply(c: Char): DetectorStates =
+      detectEnd(c)
+  }
+
+  def extensionLiteral: (LexPredicate, TokenGenerator) = {
+    val innerMatcher = new DoubleBracePairMatcher
+    (chain(innerMatcher, anyOf('}', '\n', '\r')), tokenizeExtensionLiteral(innerMatcher))
+  }
+
+  def punct: (LexPredicate, TokenGenerator) =
+    (characterMatching(c => punctuation.isDefinedAt(c.toString)),
+      p => punctuation.get(p).map(tpe => (p, tpe, null)))
+
+  def string: (LexPredicate, TokenGenerator) =
+    (chain('"', stringLexer, '"') , tokenizeString)
+
+  def stringLexer: LexPredicate = {
+    withFeedback[Option[Char]](Some('"')) {
+      case (Some('\\'), '"') => (Some('"'), Accept)
+      case (_, '"') => (Some('"'), Finished)
+      case (_, c) => (Some(c), Accept)
+    }
+  }
+
+  def comment: (LexPredicate, TokenGenerator) =
+    (chain(';', zeroOrMore(c => c != '\r' && c != '\n')),
+      {s => Some((s, TokenType.Comment, null))})
+
+  def numericLiteral: (LexPredicate, TokenGenerator) =
+    (chain(
+      characterMatching(c => Character.isDigit(c) || c == '.' || c == '-'),
+      zeroOrMore(c => validIdentifierChar(c))),
+      tokenizeLiteral)
+
+  def ident: (LexPredicate, TokenGenerator) =
+    (oneOrMore(validIdentifierChar), tokenizeIdent)
+
+  def illegalCharacter: (LexPredicate, TokenGenerator) = {
+    // if we've gotten to this point, we have a bad character
+    (aSingle(c => Accept), {s => Some((s, TokenType.Bad, "This non-standard character is not allowed.")) } )
+  }
+
+  private def tokenizeExtensionLiteral(innerMatcher: DoubleBracePairMatcher)(literalString: String): Option[(String, TokenType, AnyRef)] = {
+    if (literalString.take(2) != "{{") {
+      None
+    } else if (literalString.last == '\n' || literalString.last == '\r') {
+      Some(("", TokenType.Bad, "End of line reached unexpectedly"))
+    } else if (literalString.foldLeft((0, Option.empty[Char])) {
+          case ((nesting, lastChar), currentChar) =>
+            (lastChar, currentChar) match {
+              case (Some('{'), '{') => (nesting + 1, None)
+              case (Some('}'), '}') => (nesting - 1, None)
+              case _ => (nesting, Some(currentChar))
+            }
+        }._1 > 0)
+      Some(("", TokenType.Bad, "End of file reached unexpectedly"))
+    else
+      Some((literalString, TokenType.Extension, literalString))
+  }
+
+  private def tokenizeLiteral(literalString: String): Option[(String, TokenType, AnyRef)] = {
+    if (literalString.exists(Character.isDigit))
+      NumberParser.parse(literalString) match {
+        case Left (error) => Some((literalString, TokenType.Bad, error) )
+        case Right (literal) => Some((literalString, TokenType.Literal, literal) )
+      }
+    else
+      None
+  }
+
+  private def tokenizeIdent(identString: String): Option[(String, TokenType, AnyRef)] =
+    Some((identString, TokenType.Ident, identString.toUpperCase))
+
+  private def tokenizeString(stringText: String): Option[(String, TokenType, AnyRef)] = {
+    try {
+      if (stringText.last != '"' || stringText.takeRight(2) == "\\\"")
+        Some((s"""$stringText""", TokenType.Bad, "Closing double quote is missing"))
+      else {
+        val unescapedText = StringEscaper.unescapeString(stringText.drop(1).dropRight(1))
+        Some(( s"""$stringText""", TokenType.Literal, unescapedText))
+      }
+    } catch {
+      case ex: IllegalArgumentException => Some(( s"""$stringText""", TokenType.Bad, "Illegal character after backslash"))
+    }
+  }
+
+  def wrapInput(reader: JReader, filename: String): WrappedInput =
+    new BufferedInputWrapper(reader, 0, filename)
+
+  class BufferedInputWrapper(input: JReader, var offset: Int, val filename: String) extends WrappedInput {
+    private val buffReader: BufferedReader = new BufferedReader(input, 10000)
+
+    def nextChar: Option[Char] = {
+      val readChar = buffReader.read()
+      if (readChar == -1)
+        None
+      else
+        Some(readChar.asInstanceOf[Char])
     }
 
-  private def completeLiteral: PackratParser[TokenParseResult] =
-    literalText ^^ {
-      l => TokenParseResult(l, TokenType.Extension, l)
+    @tailrec private def longestPrefixTail(p: LexPredicate, acc: String): String =
+      nextChar match {
+        case Some(c) if p(c).continue => longestPrefixTail(p, acc + c)
+        case _ => acc
+      }
+
+    override def hasNext: Boolean = {
+      buffReader.mark(1)
+      val r = buffReader.read() != -1
+      buffReader.reset()
+      r
     }
 
-  private def literalText: PackratParser[String] =
-    "{{" ~ rep1(literalText | """[^\}]|\}[^\}]""".r) ~ "}}" ^^ {
-      case a ~ b ~ c => a + b.mkString + c
-    }
-
-  private def punct: PackratParser[TokenParseResult] =
-    punctuation.keys.map(Regex.quote).mkString("[", "", "]").r ^^ {
-      matchedPunct => TokenParseResult(matchedPunct, punctuation(matchedPunct), null)
-    }
-
-  private def comment: PackratParser[TokenParseResult] =
-    commentPattern ^^ { TokenParseResult(_, TokenType.Comment, null) }
-
-  private def number: PackratParser[TokenParseResult] =
-    numberPattern ^^ {
-      numberString => NumberParser.parse(numberString) match {
-        case Left(error) => TokenParseResult(numberString, TokenType.Bad, error)
-        case Right(literal) => TokenParseResult(numberString, TokenType.Literal, literal)
+    override def assembleToken(p: LexPredicate, f: TokenGenerator): Option[(Token, WrappedInput)] = {
+      val originalOffset = offset
+      val (prefix, remainder) = longestPrefix(p)
+      val r = prefix match {
+        case "" => None
+        case nonEmptyString => f(nonEmptyString).map {
+          case (text, tpe, tval) => (new Token(text, tpe, tval)(originalOffset, offset, filename), this)
+        }
+      }
+      r orElse {
+        buffReader.reset()
+        offset = originalOffset
+        None
       }
     }
 
-  private def ident: PackratParser[TokenParseResult] =
-    s"$identifierChar+".r ^^ {
-      s => TokenParseResult(s, TokenType.Ident, s.toUpperCase)
+    override def longestPrefix(f: LexPredicate): (String, WrappedInput) = {
+      buffReader.mark(10000)
+      val (a, b) = (longestPrefixTail(f, ""), this)
+      buffReader.reset()
+      buffReader.skip(a.length) // we always go "one too far", so we have to backup
+      offset += a.length
+      (a, b)
     }
+  }
+}
 
-  private def string: PackratParser[TokenParseResult] =
-    "\"" ~! (finishedString | unfinishedString) ^^ { case (_ ~ t) => t }
+object StandardLexer extends TokenLexer {}
 
-  private def finishedString: PackratParser[TokenParseResult] =
-    (stringPattern <~ "\"") ^^ {
-      case matchedText =>
-        try {
-          val unescapedText = StringEscaper.unescapeString(matchedText)
-          new TokenParseResult(s""""$matchedText"""", TokenType.Literal, unescapedText, unescapedText.length + 2)
-        } catch {
-          case ex: IllegalArgumentException => TokenParseResult(s""""$matchedText"""", TokenType.Bad, "Illegal character after backslash")
-        }
-    }
-
-  private def unfinishedString: PackratParser[TokenParseResult] =
-    stringPattern ^^ {
-      matchedText => TokenParseResult(s""""$matchedText""", TokenType.Bad, "Closing double quote is missing")
-    }
-
-  private def illegalCharacter: PackratParser[TokenParseResult] =
-    badChar.r ^^ {
-      s => TokenParseResult(s, TokenType.Bad, "This non-standard character is not allowed.")
-    }
-
-  def apply(input: WrappedInput): (Token, WrappedInput) =
-    handleParseResult(parse(tokenAndSkippedSpaceParser, input.input), input)
-
-  private def handleParseResult(parseResult: ParseResult[TokenParseResult], input: WrappedInput): (Token, WrappedInput) =
-    parseResult match {
-      case Success(result, reader) =>
-        val returnedTok = result.toToken(input.offset, input.filename)
-        (returnedTok, TokenInputWrapper(reader, input.offset + result.totalChars, input.filename))
-      case Error(msg, remainder) =>
-        throw new Exception(s"Error: $msg")
-      case Failure(msg, remainder) =>
-        if (remainder.atEnd)
-          (Token.Eof, TokenInputWrapper(remainder, remainder.offset, input.filename))
-        else
-          throw new Exception(s"Failure: $msg")
-    }
-
-  def wrapInput(reader: JReader, filename: String): WrappedInput =
-    TokenInputWrapper(new PackratReader(StreamReader(reader)), 0, filename)
-
-  case class TokenInputWrapper(input: Input, offset: Int, filename: String) extends WrappedInput{
-    def atEnd: Boolean = input.atEnd
+object WhitespaceSkippingLexer extends TokenLexer {
+  override def apply(input: WrappedInput): (Token, WrappedInput) = {
+    val (t, endOfToken) = super.apply(input)
+    val (_, beginningOfNextToken) = fastForwardWhitespace(endOfToken)
+    (t, beginningOfNextToken)
   }
 }
 
 object TokenLexer {
-  class TokenParseResult(text: String,
-                         tpe: TokenType,
-                         value: AnyRef,
-                         textSize: Int) {
-
-    var leadingWhitespaces = 0
-    var trailingWhitespaces = 0
-
-    def totalChars = leadingWhitespaces + textSize + trailingWhitespaces
-
-    def toToken(offset: Int, filename: String): Token =
-      new Token(text, tpe, value)(offset + leadingWhitespaces, offset + leadingWhitespaces + textSize, filename)
-
-    def consumedChars: Int = leadingWhitespaces + textSize + trailingWhitespaces
-  }
-
-  object TokenParseResult {
-    def apply(text: String, tpe: TokenType, value: AnyRef) = new TokenParseResult(text, tpe, value, text.length)
-
-    val Eof = TokenParseResult("", TokenType.Eof, "")
-  }
-
-  object StandardLexer extends TokenLexer {
-    override def skipWhitespace: Boolean = false
-
-    override private[lex] def tokenAndSkippedSpaceParser: PackratParser[TokenParseResult] =
-      (whiteSpace.? ~ token.?) ^^ {
-        case (ws ~ t) =>
-          val tok = t.getOrElse(TokenParseResult.Eof)
-          tok.leadingWhitespaces = ws.map(_.length).getOrElse(0)
-          tok
-      }
-  }
-
-  object TrailingWhitespaceSkippingLexer extends TokenLexer {
-    override def skipWhitespace: Boolean = false
-
-    override private[lex] def tokenAndSkippedSpaceParser: PackratParser[TokenParseResult] =
-      whiteSpace.? ~ (token ~ whiteSpace.?).? ^^ {
-        case leadingWs ~ Some(tok ~ trailingWs) =>
-          tok.leadingWhitespaces = leadingWs.map(_.length).getOrElse(0)
-          tok.trailingWhitespaces = trailingWs.map(_.length).getOrElse(0)
-          tok
-        case leadingWs ~ None =>
-          val tok = TokenParseResult.Eof
-          tok.leadingWhitespaces = leadingWs.map(_.length).getOrElse(0)
-          tok
-      }
-  }
-
   trait WrappedInput {
-    def atEnd: Boolean
+    def hasNext: Boolean
     def offset: Int
     def filename: String
-    def input: Reader[Char]
+    def longestPrefix(f: LexPredicate): (String, WrappedInput)
+    def assembleToken(p: LexPredicate, f: TokenGenerator): Option[(Token, WrappedInput)]
   }
 }
--- parser-core/src/main/lex/Tokenizer.scala
+++ parser-core/src/main/lex/Tokenizer.scala
@@ -3,25 +3,22 @@
 package org.nlogo.lex
 
 import org.nlogo.core, core.{ Token, TokenType },
-  TokenLexer.{ StandardLexer, TrailingWhitespaceSkippingLexer, WrappedInput }
+  TokenLexer.WrappedInput
+
+import scala.util.Success
 
 // caller's responsibility to check for TokenType.Bad!
 
 object Tokenizer extends core.TokenizerInterface {
   def tokenizeString(source: String, filename: String = ""): Iterator[Token] =
-    tokenize(new java.io.StringReader(source), filename)
+    new TokenLexIterator(StandardLexer, new java.io.StringReader(source), filename).map(_._1)
 
   def tokenize(reader: java.io.Reader, filename: String = ""): Iterator[Token] =
     new TokenLexIterator(StandardLexer, reader, filename).map(_._1)
 
-  def tokenizeWithOffset(reader: java.io.Reader, filename: String = ""): Iterator[(Token, Int)] =
-    new TokenLexIterator(StandardLexer, reader, filename).map {
-      case (t, r) => (t, r.offset)
-    }
-
   def tokenizeSkippingTrailingWhitespace(reader: java.io.Reader, filename: String = ""): Iterator[(Token, Int)] = {
     var lastOffset = 0
-    new TokenLexIterator(TrailingWhitespaceSkippingLexer, reader, filename).map {
+    new TokenLexIterator(WhitespaceSkippingLexer, reader, filename).map {
       case (t, i) =>
         val r = (t, i.offset - lastOffset - (t.end - t.start))
         lastOffset = i.offset
