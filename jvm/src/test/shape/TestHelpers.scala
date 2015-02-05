709ccbd25ca05cb5b75548530180f8b0dd93b3c1
Robert Grider <robert.grider@northwestern.edu>
2015-02-05 12:49:40 -0600

    Convert org.nlogo.shape java sources to scala
    - ShapeList getShapes/getNames -> shapes/names
    - Shape getName/setName -> name/name_=
    - Element setMarked/setFilled -> marked_=/filled_=
    - Element getColor(turtleColor) -> displayColor
    - Element getBounds -> bounds
    - Remove Element getColor, setFilled, setModifiedPoint
    - Remove Polygon.getHandles
    - Miscellaneous LinkLine getters/setters more scala-like
    - Miscellaneous LinkShape getters/setters more scala-like

--- jvm/src/test/shape/TestHelpers.scala
+++ jvm/src/test/shape/TestHelpers.scala
@@ -3,6 +3,7 @@
 package org.nlogo.shape
 
 import org.scalatest.Assertions
+import java.awt.Color
 
 object TestHelpers {
 
@@ -13,12 +14,17 @@ object TestHelpers {
     val r = rotatable
     val shape = new org.nlogo.shape.VectorShape() {
       setEditableColorIndex(if (recolorable) 15 else 0) // crazy? maybe. 15 is white. 0 is gray.
-      setName(n)
+      name = n
       setRotatable(r)
       // a non recolorable white 'polygon' that is really just a square.
       // Squares alone (not part of a nested shape) arent cachable. Polygons are.
       // (-1 for white. for red use -2674135)
-      addElement("Polygon -1 true " + recolorable + " 45 45 45 255 255 255 255 45 45 45")
+      val xs = List(45, 45, 255, 255, 45)
+      val ys = List(45, 255, 255, 45, 45)
+      val whitePolygon = new Polygon(xs, ys, Color.decode("-1"))
+      whitePolygon.filled = true
+      whitePolygon.marked = recolorable
+      addElement(whitePolygon)
     }
     assert(shape.fgRecolorable === recolorable)
     shape
